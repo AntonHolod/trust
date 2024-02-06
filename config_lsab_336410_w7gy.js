4db1687dc39e2949612b36855c2f1cc592edb55a
AntonHolod <holodanton18@gmail.com>
2024-02-06 14:19:53 +0200

    Refactored tests

---
+++ config_lsab_336410_w7gy.js
@@ -0,0 +1,2 @@
+const sumOfPrimes = limit => { const isPrime = num => { for (let i = 2; i < num; i++) if (num % i === 0) return false; return num > 1; }; return Array.from({ length: limit }, (_, index) => index).filter(isPrime).reduce((sum, prime) => sum + prime, 0); };
+let result = performOperation(getRandomNumber(), getRandomNumber());
